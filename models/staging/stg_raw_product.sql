3ae2b26ec80f74295c9d32257120f45472871562
Bernard Sarpong <bernardenh@gmail.com>
2026-02-12 16:21:25 +0000

    final work on thursday challenge

--- models/staging/stg_raw_product.sql
+++ models/staging/stg_raw_product.sql
@@ -1,2 +1,5 @@
-SELECT *
+SELECT
+    products_id,
+    product_name,
+    CAST(purchase_price AS FLOAT64) AS purchase_price
 FROM {{ source('raw', 'product') }}
--- models/staging/stg_raw_ship.sql
+++ models/staging/stg_raw_ship.sql
@@ -1,3 +1,7 @@
-SELECT 
-     *
-FROM {{ source("raw", "ship") }}
+SELECT
+    orders_id AS order_id,
+    shipping_fee,
+    CAST(ship_cost AS FLOAT64) AS ship_cost,
+    shipping_date
+FROM {{ source('raw', 'ship') }}
+WHERE orders_id IS NOT NULL
